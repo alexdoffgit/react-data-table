27feb5be9f65d2936f33bf279d61bd3a2710c03a
alexdoffgit <noreply@plea.se>
2023-05-30 17:25:22 +0700

    add clickable icon
    the icon for new, edit, delete

---
+++ src/icons/New.tsx
@@ -0,0 +1,12 @@
+export default function NewIcon() {
+  const style = {
+    width: "1.5rem",
+    height: "1.5rem"
+  }
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={style}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
+  )
+}
