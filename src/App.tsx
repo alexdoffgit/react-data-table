3110975c2aedef58f640cb2a21a7eca479d0f2b3
alexdoffgit <noreply@plea.se>
2023-05-30 17:25:36 +0700

    test table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import EditIcon from "./icons/Edit"
+import DeleteIcon from "./icons/Delete"
+const dummy = [
+  {
+    id: 1,
+    name: "andre",
+    age: 17
+  },
+  {
+    id: 2,
+    name: "william",
+    age: 19
+  }
+]
+
 function App() {
-  return (<p>Hello</p>)
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>...</th>
+        </tr>
+      </thead>
+      <tbody>
+        {dummy.map((value) => (<tr key={value.id}>
+          <td>{value.name}</td>
+          <td>{value.age}</td>
+          <td>
+            <EditIcon />
+            <DeleteIcon />
+          </td>
+        </tr>))}
+      </tbody>
+    </table>
+  )
 }
 
 export default App
