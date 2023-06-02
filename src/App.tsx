24910927da6759fb278ef4f9b8ec23aa031409ed
alexdoffgit <noreply@plea.se>
2023-06-02 07:59:36 +0700

    implement new and cancel button

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,87 @@
-import EditIcon from "./icons/Edit"
-import DeleteIcon from "./icons/Delete"
-const dummy = [
-  {
-    id: 1,
-    name: "andre",
-    age: 17
-  },
-  {
-    id: 2,
-    name: "william",
-    age: 19
+import EditIcon from "./icons/Edit";
+import DeleteIcon from "./icons/Delete";
+import NewIcon from "./icons/New";
+import { useState } from "react";
+import AcceptIcon from "./icons/Accept";
+import CancelIcon from "./icons/Cancel";
+
+type Mode = "read" | "edit" | "new"
+
+function useAppState() {
+  const [state, setState] = useState([
+    {
+      id: 1,
+      name: "andre",
+      age: 17,
+    },
+    {
+      id: 2,
+      name: "william",
+      age: 19,
+    },
+  ])
+  const [mode, setMode] = useState<Mode>("read")
+
+  return {
+    state,
+    setState,
+    mode,
+    setMode
   }
-]
+}
 
 function App() {
+  const { state, setState, mode, setMode } = useAppState()
+
   return (
     <table className="table">
       <thead>
         <tr>
           <th>Name</th>
           <th>Age</th>
-          <th>...</th>
+          <th>
+            <button onClick={() => setMode("new")}>
+              <NewIcon />
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {dummy.map((value) => (<tr key={value.id}>
-          <td>{value.name}</td>
-          <td>{value.age}</td>
-          <td>
-            <EditIcon />
-            <DeleteIcon />
-          </td>
-        </tr>))}
+        {state.map((value) => (
+          <tr key={value.id}>
+            <td>{value.name}</td>
+            <td>{value.age}</td>
+            <td>
+              <button>
+                <EditIcon />
+              </button>
+              <button>
+                <DeleteIcon />
+              </button>
+            </td>
+          </tr>
+        ))}
+        {mode === "new" &&
+          <tr>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <button>
+                <AcceptIcon />
+              </button>
+              <button onClick={() => setMode("read")}>
+                <CancelIcon />
+              </button>
+            </td>
+          </tr>
+        }
       </tbody>
     </table>
-  )
+  );
 }
 
-export default App
+export default App;
