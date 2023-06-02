b0cb6b72d93231d974d66d243e7a8ef90e362844
alexdoffgit <noreply@plea.se>
2023-06-02 07:59:21 +0700

    add two icon

---
+++ src/icons/Accept.tsx
@@ -0,0 +1,23 @@
+export default function AcceptIcon() {
+  const style = {
+    width: "1.5rem",
+    height: "1.5rem",
+  };
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      style={style}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+      />
+    </svg>
+  );
+}
---
+++ src/icons/Cancel.tsx
@@ -0,0 +1,23 @@
+export default function CancelIcon() {
+  const style = {
+    width: "1.5rem",
+    height: "1.5rem",
+  };
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      style={style}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
